a88ca3908b61c96b1903a71c4832974c7b5534a7
alesoaresdecarvalho <alessandr.carvalh@gmail.com>
2023-09-12 19:56:19 -0300

    atualização

--- blogPessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ blogPessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -3,6 +3,7 @@ package com.generation.blogpessoal.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -51,7 +53,27 @@ public class BasicSecurityConfig {
 	
 	 @Bean
 	    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		 				
+		 	
+		 http
+         .sessionManagement()
+         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+         .and().csrf().disable()
+         .cors();
+
+ http
+         .authorizeHttpRequests((auth) -> auth
+                 .requestMatchers("/usuarios/logar").permitAll()
+                 .requestMatchers("/usuarios/cadastrar").permitAll()
+                 .requestMatchers("/error/**").permitAll()
+                 .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                 .anyRequest().authenticated())
+         .authenticationProvider(authenticationProvider())
+         .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+         .httpBasic();
+
+ return http.build();
+
+}
 		 				
 		 		
 		 
