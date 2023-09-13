eaf6e5102d66a57e70cb233087ea151bbd8e7b80
alesoaresdecarvalho <alessandr.carvalh@gmail.com>
2023-09-13 17:08:26 -0300

    atualização

--- blogPessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ blogPessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,65 +20,57 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 public class BasicSecurityConfig {
-	
-	@Autowired
-	private JwtAuthFilter authFilter;
-	
-	
-	@Bean
-	UserDetailsService userDetailsService() {
-	
-	return new UserDetailsServiceImpl();
-	
-	}
-	
-	@Bean
-	PasswordEncoder passwordEncoder() {
-	return new BCryptPasswordEncoder();
-	}
-	
-	@Bean
-	AuthenticationProvider authenticationProvider() {
-	DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-	authenticationProvider.setUserDetailsService (userDetailsService());
-	authenticationProvider.setPasswordEncoder (passwordEncoder());
-	return authenticationProvider;
-	}
-	
-	@Bean
-	AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration)
-	throws Exception {
-	
-	return authenticationConfiguration.getAuthenticationManager();
-	}
-	
-	 @Bean
-	    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		 	
-		 http
-         .sessionManagement()
-         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-         .and().csrf().disable()
-         .cors();
 
- http
-         .authorizeHttpRequests((auth) -> auth
-                 .requestMatchers("/usuarios/logar").permitAll()
-                 .requestMatchers("/usuarios/cadastrar").permitAll()
-                 .requestMatchers("/error/**").permitAll()
-                 .requestMatchers(HttpMethod.OPTIONS).permitAll()
-                 .anyRequest().authenticated())
-         .authenticationProvider(authenticationProvider())
-         .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-         .httpBasic();
+    @Autowired
+    private JwtAuthFilter authFilter;
 
- return http.build();
+    @Bean
+    UserDetailsService userDetailsService() {
 
-}
-		 				
-		 		
-		 
-		 		
-		 
-	 }
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable()
+                .cors();
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/usuarios/logar").permitAll()
+                        .requestMatchers("/usuarios/cadastrar").permitAll()
+                        .requestMatchers("/error/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated())
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic();
+
+        return http.build();
+
+    }
+
 }
